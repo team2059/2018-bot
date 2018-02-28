5dd964806badf26f9a5db1e540dd28131fdaab99
Achintya Kumar <achintyasi@gmail.com>
2018-02-27 19:33:46 -0500

    testing new vision code

---
+++ src/org/usfirst/frc/team2059/robot/subsystems/VisionHelper.java
@@ -0,0 +1,37 @@
+package org.usfirst.frc.team2059.robot.subsystems;
+
+import edu.wpi.first.networktables.*;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class VisionHelper extends Subsystem{
+	
+	NetworkTableInstance inst = NetworkTableInstance.getDefault();
+	NetworkTable table = inst.getTable("dataTable");
+	NetworkTableEntry angleEntry = table.getEntry("");
+	NetworkTableEntry distEntry = table.getEntry("");
+	
+	@Override
+	protected void initDefaultCommand() {
+	}
+	
+	public VisionHelper() {
+		inst.startClientTeam(2059);
+		inst.startDSClient();
+		
+	}
+	
+	
+	
+	public double getAngle() {
+		return angleEntry.getDouble(0.0);
+	}
+	
+	public double getDistance() {
+		return distEntry.getDouble(0.0);
+	}
+	
+	public double getDistanceFromStop() {
+		return 1;
+	}
+
+}
